d78bb91b64ab254761a2f910c4bdff4ba4b00191
Jaime Martinez <jmartinez@gitlab.com>
2021-01-08 15:16:11 +1100

    Add my custom plugins

---
+++ custom/plugins/aliases/aliases.plugin.zsh
@@ -0,0 +1,50 @@
+
+# aliases
+alias go='nocorrect go'
+alias terraform='nocorrect terraform'
+alias tf='terraform'
+alias git='nocorrect git'
+alias bit='nocorrect bit'
+
+alias bundle='nocorrect bundle'
+alias jm='cd $JM'
+alias drlint='docker run --rm -v $PWD:/go/app -w /go/app golangci/golangci-lint:v1.27.0 golangci-lint run'
+#eval $(thefuck --alias)
+# You can use whatever you want as an alias, like for Mondays:
+#eval $(thefuck --alias FUCK)
+alias mk='make'
+alias code='codium'
+#GITLAB STUFF
+alias glsite='NO_CONTRACTS=true bundle exec middleman'
+alias grr='gitlab-runner'
+alias ee='cd $EE'
+alias ce='cd $CE'
+alias dev='cd $HOME/dev'
+alias gitlab='cd $HOME/dev/gitlab'
+alias ifactive='ifconfig | pcregrep -M -o '\''^[^\t:]+(?=:([^\n]|\n\t)*status: active)'\'
+alias ip='ifconfig | grep "inet " | grep -v 127.0.0.1 | cut -d\  -f2'
+alias update_local_ip='sed -ie "s/LOCAL_IP=.*/LOCAL_IP=$(ip)/" ~/.zshrc'
+
+
+alias reload='source $HOME/.zshrc'
+alias drmf='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
+alias drmi='docker rmi $(docker images | grep "<none>" | awk '\''{print $3 }'\'')'
+alias drlogin='eval $(aws ecr get-login --no-include-email --region ap-southeast-2 | sed '\''s|https://||'\'')'
+alias gs='git status '
+alias ga='git add '
+alias gb='git branch '
+alias gc='git commit'
+alias gd='git diff'
+alias gco='git checkout '
+alias master='git checkout master'
+alias masterp='git checkout master && git pull'
+alias gp='git push'
+alias glg='git log --graph --pretty=format:'"'"'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"'"' --abbrev-commit'
+
+alias h='cd $HOME'
+
+alias uuidgen='uuidgen | tr "[:upper:]" "[:lower:]"'
+alias nyan='nyancat'
+
+
+alias hpr='hub pull-request'
---
+++ custom/plugins/myenv/myenv.plugin.zsh
@@ -0,0 +1,20 @@
+export GPG_TTY=$(tty)
+#export JAVA_HOME=$(/usr/libexec/java_home)
+#export GOPATH=$HOME/go
+#export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOPATH/bin:$JAVA_HOME/bin:/usr/local/Cellar/libpq/9.6.5/bin:$HOME/bin:~/Library/Python/3.6/bin/:/Library/Frameworks/Python.framework/Versions/3.6/bin
+export JM=$GOPATH/src/gitlab.com/jaime
+# Customise the Powerlevel9k prompts
+export POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(user dir newline vcs) 
+export POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status time ssh) 
+export POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_last
+
+
+export POWERLEVEL9K_USER_ICON="\ue724"
+export POWERLEVEL9K_USER_DEFAULT_FOREGROUND="cyan"
+export POWERLEVEL9K_USER_DEFAULT_BACKGROUND="none"
+
+export POWERLEVEL9K_TIME_FOREGROUND="orange1"
+export POWERLEVEL9K_TIME_BACKGROUND="none"
+
+export POWERLEVEL9K_MODE='nerdfont-complete'
